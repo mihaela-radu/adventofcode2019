e4b92092520808778021761223a99e0e7894593c
Mihaela Radu <mihaela.radu@sitecore.com>
2019-12-04 12:04:36 +0000

    Day3 part 2 done.

---
+++ Day3/Move.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace Day3
+{
+    public class Move
+    {
+        public Move(Point point, long steps)
+        {
+            Point = point;
+            Steps = steps;
+        }
+
+        public Point Point { get; set; }
+
+        public long Steps { get; set; }
+    }
+}
